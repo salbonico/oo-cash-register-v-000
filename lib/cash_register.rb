e31b0573a2717fc30601c40df6e839a20c40f5df
Steve Albonico <salbonico@gmail.com>
2019-01-18 01:16:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,6 +19,11 @@ self.total += price*quantity
 end
 
 def apply_discount
+
+if @discount == 0
+  puts "There is no discount to apply."
+end
+  
 money_off = self.total * (@discount/100.0)
 self.total -= money_off
 return "After the discount, the total comes to $#{@total.to_i}."
